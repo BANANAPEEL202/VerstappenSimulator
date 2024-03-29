5471b09c307064f0a6a0673ca6433ae2db4e5506
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-29 09:29:47 -0400

    Initial commit

---
+++ images/redbull.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 redbull redbull.png 
+ * Time-stamp: Thursday 03/28/2024, 22:50:15
+ * 
+ * Image Information
+ * -----------------
+ * redbull.png 19@50
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef REDBULL_H
+#define REDBULL_H
+
+extern const unsigned short redbull[950];
+#define REDBULL_SIZE 1900
+#define REDBULL_LENGTH 950
+#define REDBULL_WIDTH 19
+#define REDBULL_HEIGHT 50
+
+#endif
+
---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "gba.h"
+
+// TODO: Create any necessary structs
+
+struct Car {
+    int x;
+    int y;
+    const u16* image;
+};
+
+/*
+* For example, for a Snake game, one could be:
+*
+* struct snake {
+*   int heading;
+*   int length;
+*   int row;
+*   int col;
+* };
+*
+* Example of a struct to hold state machine data:
+*
+* struct state {
+*   int currentState;
+*   int nextState;
+* };
+*
+*/
+
+#endif
